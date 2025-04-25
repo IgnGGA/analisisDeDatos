2ccd83ab18299409640f8fd24e3a363dfbef41dc
Ignacio Gamboa <ign.gga@outlook.cl>
2025-04-24 21:24:34 -0400

    proyectoPython.ipynb es el documento final de trabajo

---
+++ ExtracciónDeDatosBursátilesMedianteUnaBibliotecaDePython/eddb.py
@@ -0,0 +1,15 @@
+import yfinance as yf
+import pandas as pd
+
+apple = yf.Ticker("AAPL")
+
+wget https://cf-courses-data.s3.us.cloud-object-storage.appdomain.cloud/IBMDeveloperSkillsNetwork-PY0220EN-SkillsNetwork/data/apple.json
+
+import json
+with open('apple.json') as json_file:
+    apple_info = json.load(json_file)
+    # Print the type of data variable    
+    #print("Type:", type(apple_info))
+apple_info
+
+apple_info['country']
